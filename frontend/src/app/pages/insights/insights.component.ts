ef36dea6fed1a3ff1c33ccad7a9c87e578521375
atimmer <atimmer@terpmail.umd.edu>
2026-04-29 19:31:39 -0400

    wire Read Full Insight button to article route

--- frontend/src/app/pages/insights/insights.component.ts
+++ frontend/src/app/pages/insights/insights.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-insights',
   standalone: true,
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './insights.component.html',
   styleUrl: './insights.component.scss'
 })
